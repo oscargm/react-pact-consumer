f21948a5466436b44b947bbc28328886330ce663
admin <oscar.garcia.og3@roche.com>
2019-10-29 10:42:34 +0100

    port change

--- pact/setup.js
+++ pact/setup.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const Pact = require("@pact-foundation/pact").Pact;
+const path = require('path');
+const Pact = require('@pact-foundation/pact').Pact;
 
-global.port = 82;
+global.port = 80;
 global.provider = new Pact({
   cors: true,
   port: global.port,
-  log: path.resolve(process.cwd(), "logs", "pact.log"),
-  loglevel: "debug",
-  dir: path.resolve(process.cwd(), "pacts"),
+  log: path.resolve(process.cwd(), 'logs', 'pact.log'),
+  loglevel: 'debug',
+  dir: path.resolve(process.cwd(), 'pacts'),
   spec: 2,
-  pactfileWriteMode: "update",
-  consumer: "hero-consumer",
-  provider: "hero-provider",
-  host: "pact_broker"
+  pactfileWriteMode: 'update',
+  consumer: 'hero-consumer',
+  provider: 'hero-provider',
+  host: 'pact_broker'
 });
