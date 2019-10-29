2301c1517599ca002b0d52d82827facfa568339d
admin <oscar.garcia.og3@roche.com>
2019-10-29 11:01:18 +0100

    pact server config change

--- pact/publish.js
+++ pact/publish.js
@@ -4,9 +4,9 @@ let path = require('path');
 let opts = {
   providerBaseUrl: 'http://pact_broker:80',
   pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')],
-  pactBroker: 'https://adesso.pact.dius.com.au/',
-  pactBrokerUsername: process.env.PACT_USERNAME,
-  pactBrokerPassword: process.env.PACT_PASSWORD,
+  pactBroker: 'http://pact_broker:80',
+  // pactBrokerUsername: process.env.PACT_USERNAME,
+  // pactBrokerPassword: process.env.PACT_PASSWORD,
   consumerVersion: '2.0.0'
 };
 
