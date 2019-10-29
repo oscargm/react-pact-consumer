bf25288729bbe8993e7775605a783b00a4565c42
admin <oscar.garcia.og3@roche.com>
2019-10-29 10:13:08 +0100

    server alias

--- pact/publish.js
+++ pact/publish.js
@@ -1,15 +1,15 @@
-let publisher = require("@pact-foundation/pact-node");
-let path = require("path");
+let publisher = require('@pact-foundation/pact-node');
+let path = require('path');
 
 let opts = {
-  providerBaseUrl: "http://localhost:80",
-  pactFilesOrDirs: [path.resolve(process.cwd(), "pacts")],
-  pactBroker: "https://adesso.pact.dius.com.au/",
+  providerBaseUrl: 'http://pact_broker:80',
+  pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')],
+  pactBroker: 'https://adesso.pact.dius.com.au/',
   pactBrokerUsername: process.env.PACT_USERNAME,
   pactBrokerPassword: process.env.PACT_PASSWORD,
-  consumerVersion: "2.0.0"
+  consumerVersion: '2.0.0'
 };
 
 publisher
   .publishPacts(opts)
-  .then(() => console.log("Pacts successfully published"));
+  .then(() => console.log('Pacts successfully published'));
