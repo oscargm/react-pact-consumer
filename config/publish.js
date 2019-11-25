915c892d8d177dc899770cf2b114bec9dd8916b3
admin <oscar.garcia.og3@roche.com>
2019-11-25 11:21:44 +0100

    publish

--- config/publish.js
+++ config/publish.js
@@ -2,9 +2,9 @@ let publisher = require('@pact-foundation/pact-node');
 let path = require('path');
 
 let opts = {
-  providerBaseUrl: 'http://pact_broker:80',
+  providerBaseUrl: 'http://localhost:82',
   pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')],
-  pactBroker: 'http://pact_broker:80',
+  pactBroker: 'http://localhost:82',
   // pactBrokerUsername: process.env.PACT_USERNAME,
   // pactBrokerPassword: process.env.PACT_PASSWORD,
   consumerVersion: '2.0.0'
